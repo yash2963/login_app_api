1bede5840a9ab2e57dce6f9d11f4d533aa3d294c
Yash <k730453@gmail.com>
2026-03-23 01:35:44 +0530

    Update forgotpassword.py

--- forgotpassword.py
+++ forgotpassword.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter,Depends,HTTPException,status
-from sqlalchemy.orm import Session
+# from sqlalchemy.orm import Session
 # from database import get_db
-import schemas
-from sqlalchemy import or_
+# import schemas
+# from sqlalchemy import or_
 
 router = APIRouter(prefix='/users',tags=["Forgot-Password"])
 
